0bfafce1e14b9642d9ee1f66ed6527dbf3c8299f
Ofer Zadikario <ofer.zadikario@gmail.com>
2023-11-26 11:45:52 +0200

    Add Signup UI example

--- Trivia_Stage1/UI/TriviaScreensImp.cs
+++ Trivia_Stage1/UI/TriviaScreensImp.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Trivia_Stage1.UI
@@ -22,9 +24,66 @@ namespace Trivia_Stage1.UI
         }
         public bool ShowSignup()
         {
-            Console.WriteLine("Not implemented yet! Press any key to continue...");
-            Console.ReadKey(true); 
-            return true;
+            //Logout user if anyone is logged in!
+            //A reference to the logged in user should be stored as a member variable
+            //in this class! Example:
+            //this.currentyPLayer == null
+
+            //Loop through inputs until a user/player is created or 
+            //user choose to go back to menu
+            char c = ' ';
+            while (c != 'B' && c != 'b' /*&& this.currentyPLayer == null*/)
+            {
+                //Clear screen
+                CleareAndTtile("Signup");
+
+                Console.Write("Please Type your email: ");
+                string email = Console.ReadLine();
+                while (!IsEmailValid(email))
+                {
+                    Console.Write("Bad Email Format! Please try again:");
+                    email = Console.ReadLine();
+                }
+
+                Console.Write("Please Type your password: ");
+                string password = Console.ReadLine();
+                while (!IsPasswordValid(password))
+                {
+                    Console.Write("password must be at least 4 characters! Please try again: ");
+                    password = Console.ReadLine();
+                }
+
+                Console.Write("Please Type your Name: ");
+                string name = Console.ReadLine();
+                while (!IsNameValid(name))
+                {
+                    Console.Write("name must be at least 3 characters! Please try again: ");
+                    name = Console.ReadLine();
+                }
+
+
+                Console.WriteLine("Connecting to Server...");
+                /* Create instance of Business Logic and call the signup method
+                 * For example:
+                try
+                {
+                    TriviaDBContext db = new TriviaDBContext();
+                    this.currentyPLayer = db.Signup(email, password, name);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to signup! Email may already exist in DB!");
+                }
+                
+                */
+
+                //Provide a proper message for example:
+                Console.WriteLine("Press (B)ack to go back or any other key to signup again...");
+                //Get another input from user
+                c = Console.ReadKey(true).KeyChar;
+            }
+            //return true if signup suceeded!
+            return (false);
         }
 
         public void ShowAddQuestion()
@@ -48,5 +107,32 @@ namespace Trivia_Stage1.UI
             Console.WriteLine("Not implemented yet! Press any key to continue...");
             Console.ReadKey(true);
         }
+
+        //Private helper methodfs down here...
+        private void CleareAndTtile(string title)
+        {
+            Console.Clear();
+            Console.WriteLine($"\t\t\t\t\t{title}");
+            Console.WriteLine();
+        }
+
+        private bool IsEmailValid(string emailAddress)
+        {
+            var pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+            var regex = new Regex(pattern);
+            return regex.IsMatch(emailAddress);
+        }
+
+        private bool IsPasswordValid(string password)
+        {
+            return password != null && password.Length >= 3;
+        }
+
+        private bool IsNameValid(string name)
+        {
+            return name != null && name.Length >= 3;
+        }
+
     }
 }
