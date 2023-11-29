62c2beddbc11aa273e0b0025567fe16ccf391258
User <User@LAB3-3>
2023-11-29 09:21:09 +0200

    ooo

---
+++ Trivia_Stage1/Models/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Trivia_Stage1.Models;
+
+public partial class Player
+{
+    [Key]
+    public int PlayerId { get; set; }
+
+    [StringLength(30)]
+    public string Mail { get; set; } = null!;
+
+    [StringLength(20)]
+    public string Name { get; set; } = null!;
+
+    public int RankId { get; set; }
+
+    public int Points { get; set; }
+
+    [InverseProperty("Player")]
+    public virtual ICollection<Question> Questions { get; } = new List<Question>();
+
+    [ForeignKey("RankId")]
+    [InverseProperty("Players")]
+    public virtual Rank Rank { get; set; } = null!;
+}
---
+++ Trivia_Stage1/Models/TriviaDbContext.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Trivia_Stage1.Models;
+
+public partial class TriviaDbContext : DbContext
+{
+    public TriviaDbContext()
+    {
+    }
+
+    public TriviaDbContext(DbContextOptions<TriviaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Player> Players { get; set; }
+
+    public virtual DbSet<Question> Questions { get; set; }
+
+    public virtual DbSet<Rank> Ranks { get; set; }
+
+    public virtual DbSet<Status> Statuses { get; set; }
+
+    public virtual DbSet<Subject> Subjects { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server = localhost\\SQLEXPRESS; Database=TriviaGame; Trusted_Connection=true; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Player>(entity =>
+        {
+            entity.HasKey(e => e.PlayerId).HasName("PK__Players__4A4E74C8AB2BFF0F");
+
+            entity.HasOne(d => d.Rank).WithMany(p => p.Players)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_RankId");
+        });
+
+        modelBuilder.Entity<Question>(entity =>
+        {
+            entity.HasKey(e => e.QuestionId).HasName("PK__Question__0DC06FACCFC7143F");
+
+            entity.HasOne(d => d.Player).WithMany(p => p.Questions)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_PlayerId");
+
+            entity.HasOne(d => d.Status).WithMany(p => p.Questions)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_StatusId");
+
+            entity.HasOne(d => d.Subject).WithMany(p => p.Questions)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_SubjectId");
+        });
+
+        modelBuilder.Entity<Rank>(entity =>
+        {
+            entity.HasKey(e => e.RankId).HasName("PK__Ranks__B37AF876513DDC08");
+        });
+
+        modelBuilder.Entity<Status>(entity =>
+        {
+            entity.HasKey(e => e.StatusId).HasName("PK__Status__C8EE206316860C1C");
+        });
+
+        modelBuilder.Entity<Subject>(entity =>
+        {
+            entity.HasKey(e => e.SubjectId).HasName("PK__Subject__AC1BA3A8354CEAB1");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
