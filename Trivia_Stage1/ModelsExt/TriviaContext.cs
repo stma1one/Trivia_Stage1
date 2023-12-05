3cfa48489809accf4f7001b32d884641ba897a0f
User <User@LAB3-3>
2023-12-05 13:28:50 +0200

    added DoesUserExist and GetUserByEmail

---
+++ Trivia_Stage1/ModelsExt/TriviaContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trivia_Stage1.Models
+{
+    public partial class TriviaContext
+    {
+        public bool DoesUserExist(string email)
+        {
+            return this.Users.Where(user => user.Email == email).Any();
+        }
+        public User GetUserByEmail(string email)
+        {
+            try
+            {
+                return this.Users.Where(user => user.Email == email).First();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
